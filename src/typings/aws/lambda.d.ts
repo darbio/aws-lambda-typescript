cdbad91d30a1cffcaaa07f2d1a4c7e70ccb9c2c0
James Darbyshire <darbio@users.noreply.github.com>
2016-01-06 16:42:44 +1100

    Update lambda.d.ts

--- src/typings/aws/lambda.d.ts
+++ src/typings/aws/lambda.d.ts
@@ -7,7 +7,13 @@
 *                                               *
 ************************************************/
 
+// Context
+// http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
 interface Context {
+    // Properties
+    // TODO
+    
+    // Functions
     succeed(result?: Object): void;
     fail(error?: Error): void;
     done(error?: Error, result?: Object): void; // result must be JSON.stringifyable
